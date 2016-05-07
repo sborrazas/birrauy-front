ddf314723befa5c2ea0b5fd686cfa3d365688e24
Sebastian Borrazas <seba.borrazas@gmail.com>
2016-05-07 13:46:29 -0300

    Add FB events fetching

---
+++ README.md
@@ -0,0 +1,8 @@
+# BirraUY
+
+## Start the server
+
+```
+$ FB_ACCESS_TOKEN=<token> npm run start # To run the node server
+$ npm webpack-watch # To watch & compile js assets
+```
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,21 @@
 var path = require("path")
   , Express = require("express")
+  , graph = require("fbgraph")
   , app = Express()
   , PATH_STYLES = path.resolve(__dirname, "../client/styles")
   , PATH_IMAGES = path.resolve(__dirname, "../client/images")
   , PATH_SCRIPTS = path.resolve(__dirname, "../client/scripts")
   , PATH_FONTS = path.resolve(__dirname, "../client/fonts")
   , server = null;
+
+const fbAccessToken = process.env.FB_ACCESS_TOKEN;
+
+if (!fbAccessToken) {
+  console.log("`FB_ACCESS_TOKEN` ENV variable not provided.");
+  process.exit(1);
+}
+
+graph.setAccessToken(fbAccessToken);
 
 app.use("/styles", Express.static(PATH_STYLES));
 app.use("/images", Express.static(PATH_IMAGES));
@@ -14,6 +24,22 @@ app.use("/fonts", Express.static(PATH_FONTS));
 
 app.get("/404", function (req, res) {
   res.sendFile(path.resolve(__dirname, "../client/404.html"));
+});
+
+app.get("/api/events", function (req, res) {
+  graph.get(
+    "/CervezaArtesanalUY/events",
+    function (err, response) {
+      if (response && !response.error) {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(response.data));
+      }
+      else {
+        res.statusCode = 500;
+        res.end("Internal server error.");
+      }
+    }
+  );
 });
 
 app.get("*", function (req, res) {
